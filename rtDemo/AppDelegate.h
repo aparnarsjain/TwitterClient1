7f4bc7bb48da0e08128cbab53d0b46afb7107455
Aparna Jain <aparna@Admins-MacBook-Pro-2.local>
2014-05-28 12:42:43 -0700

    Initial Commit

---
+++ rtDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  rtDemo
+//
+//  Created by Aparna Jain on 5/28/14.
+//  Copyright (c) 2014 FOX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ rtDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  rtDemo
+//
+//  Created by Aparna Jain on 5/28/14.
+//  Copyright (c) 2014 FOX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
